2cf66ccc6b31d52525c4912a8cde31a98ca5fc50
NorseRalph <89695194+NorseRalph@users.noreply.github.com>
2023-05-16 22:06:28 +0200

    gay

--- ui-application/src/api/api.ts
+++ ui-application/src/api/api.ts
@@ -1,14 +1,27 @@
-import React from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080'; // Replace this with your Spring Boot application's URL and port
+
+const encodedCredentials = btoa('admin@example.com:pass');
+
 
 const instance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: 'http://localhost:8080', // Replace this with your server URL
   headers: {
     'Content-Type': 'application/json',
+    'Authentication: ': 'Basic YWRtaW5AZXhhbXBsZS5jb206cGFzcw=='
   },
 });
 
-export default instance;
+// Intercept the request to make sure the Authorization token is injected into the header.
+instance.interceptors.request.use((config) => {
+  const user = JSON.parse(localStorage.getItem('user') || '{}');
 
+  if (user && user.authdata) {
+    config.headers = config.headers || {};
+    config.headers['Authorization'] = 'Basic ' + user.authdata;
+  }
+
+  return config;
+});
+
+export default instance;
